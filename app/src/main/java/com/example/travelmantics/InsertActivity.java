4d53fc025309c5052e9848bf9020ce6108ff1a72
Letlhogonolo Theodore Obonye <ltobonye@gmail.com>
2019-08-02 22:35:37 +0200

    add save button and form fields

--- app/src/main/java/com/example/travelmantics/InsertActivity.java
+++ app/src/main/java/com/example/travelmantics/InsertActivity.java
@@ -1,7 +1,9 @@
 package com.example.travelmantics;
 
-//import android.support.v7.app.AppCompatActivity;
+//import android.support.v4.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuInflater;
 
 import androidx.appcompat.app.AppCompatActivity;
 
@@ -11,5 +13,12 @@ public class InsertActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_insert);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.save_menu, menu);
+        return true;
     }
 }
