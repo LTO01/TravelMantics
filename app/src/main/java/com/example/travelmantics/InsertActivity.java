f884e619d92ca278453cd6dfa0c514d3b60b5553
Letlhogonolo Theodore Obonye <ltobonye@gmail.com>
2019-08-03 16:45:20 +0200

    save to firebase

--- app/src/main/java/com/example/travelmantics/InsertActivity.java
+++ app/src/main/java/com/example/travelmantics/InsertActivity.java
@@ -1,18 +1,34 @@
 package com.example.travelmantics;
 
 //import android.support.v4.app.AppCompatActivity;
+
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuInflater;
+import android.view.MenuItem;
+import android.widget.EditText;
+import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+
 public class InsertActivity extends AppCompatActivity {
+    private FirebaseDatabase mFirebaseDatabase;
+    private DatabaseReference mDatabaseReference;
+    EditText txtTitle, txtDescription, txtPrice;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_insert);
+        mFirebaseDatabase = mFirebaseDatabase.getInstance();
+        mDatabaseReference = mFirebaseDatabase.getReference().child("traveldeals");
+        txtTitle = (EditText) findViewById(R.id.textTitle);
+        txtDescription = (EditText) findViewById(R.id.textDescription);
+        txtPrice = (EditText) findViewById(R.id.textPrice);
     }
 
     @Override
@@ -20,5 +36,33 @@ public class InsertActivity extends AppCompatActivity {
         MenuInflater inflater = getMenuInflater();
         inflater.inflate(R.menu.save_menu, menu);
         return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            case R.id.save_menu:
+                saveDeal();
+                Toast.makeText(this, "Deal saved", Toast.LENGTH_LONG).show();
+                clean();
+                return true;
+            default:
+                return super.onOptionsItemSelected(item);
+        }
+
+    }
+
+    private void clean() {
+        txtPrice.setText("");
+        txtDescription.setText("");
+        txtTitle.setText("");
+    }
+
+    private void saveDeal() {
+        String title = txtTitle.getText().toString();
+        String description = txtDescription.getText().toString();
+        String price = txtPrice.getText().toString();
+        TravelDeal deal = new TravelDeal(title, description, price, "");
+        mDatabaseReference.push().setValue(deal);
     }
 }
