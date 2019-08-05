694fc5d6fdcb611fffdfac29d0dcf3f6f96456b5
Letlhogonolo Theodore Obonye <ltobonye@gmail.com>
2019-08-05 06:45:25 +0200

    refactor: add authentication members

--- app/src/main/java/com/example/travelmantics/FirebaseUtil.java
+++ app/src/main/java/com/example/travelmantics/FirebaseUtil.java
@@ -1,5 +1,6 @@
 package com.example.travelmantics;
 
+import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 
@@ -9,15 +10,20 @@ public class FirebaseUtil {
     public static FirebaseDatabase mFirebaseDatabase;
     public static DatabaseReference mDatabaseReference;
     public static FirebaseUtil firebaseUtil;
+    public static FirebaseAuth mFirebaseAuth;
+    public static FirebaseAuth.AuthStateListener m
     public static ArrayList<TravelDeal> mDeals;
 
-    private FirebaseUtil() {};
-    public static void openFBReference(String ref){
-        if (firebaseUtil == null){
+    private FirebaseUtil() {
+    };
+
+    public static void openFBReference(String ref) {
+        if (firebaseUtil == null) {
             firebaseUtil = new FirebaseUtil();
             mFirebaseDatabase = mFirebaseDatabase.getInstance();
-            mDeals = new ArrayList<TravelDeal>();
+
         }
+        mDeals = new ArrayList<TravelDeal>();
         mDatabaseReference = mFirebaseDatabase.getReference().child(ref);
     }
 }
